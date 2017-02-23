311235acdfd71f73e1be3825af86f3b6c388e844
Matthew Ratzke <matt@degree9.io>
2017-02-23 16:59:26 -0700

    Fix duplicate files (#48)
    * added include option
    
    * small changes
    
    * strip build and pre-release prefix
    
    * use string replace
    
    * remove read-string
    
    * Fix inc/dec
    
    * try fixing inc dec again
    
    * fix typo
    
    * rename ns
    
    * generate ns
    
    * cleanup
    
    * rename helpers
    
    * wip
    
    * Update README.md
    
    * fix generator
    
    * fix deps
    
    * fix generator path
    
    * added include option
    
    * small changes
    
    * strip build and pre-release prefix
    
    * use string replace
    
    * remove read-string
    
    * Fix inc/dec
    
    * try fixing inc dec again
    
    * fix typo
    
    * rename ns
    
    * generate ns
    
    * cleanup
    
    * rename helpers
    
    * wip
    
    * fix generator
    
    * fix deps
    
    * Fix Conflicts
    
    * add exclusion for version file

--- src/degree9/boot_semver.clj
+++ src/degree9/boot_semver.clj
@@ -147,9 +147,11 @@
           (util/dbug "Generating Namespace File...: %s\n" ns-file)
           (doto ns-file io/make-parents
             (spit (str "(ns " gen-ns ")\n" "(def version \"" version "\")\n"))))
+        (prn (-> semver-file io/file .getParent io/file .getAbsolutePath))
         (cond-> fs
           include (-> (boot/add-resource (-> semver-file io/file .getParent io/file)
-                        :include #{#"version.properties"})
+                        :include #{#"version.properties"}
+                        :exclude #{#"\.*\version.properties"})
                       boot/commit!)
           gen-ns  (-> (boot/add-resource tmp)
                       boot/commit!))))))
